cebc25f1d1341c7bef8c64fd8ed57c12eb60d9fd
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-06-02 22:14:22 +0900

    Updates

---
+++ precache-manifest.6a36c74a4d799f1de25dddef35ceb494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbfc4da1496e9a96ac2dc70a0974a96",
+    "url": "/parkflix/index.html"
+  },
+  {
+    "revision": "8dfd0adbaca688662c36",
+    "url": "/parkflix/static/js/2.7e117253.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/parkflix/static/js/2.7e117253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fefe07223d27d0921ae",
+    "url": "/parkflix/static/js/main.2e96e3f4.chunk.js"
+  },
+  {
+    "revision": "0b1036c887a1bb908bff",
+    "url": "/parkflix/static/js/runtime-main.8f21a437.js"
+  },
+  {
+    "revision": "b399700097df15b75cec573b01023be2",
+    "url": "/parkflix/static/media/noPosterSmall.b3997000.png"
+  }
+]);
